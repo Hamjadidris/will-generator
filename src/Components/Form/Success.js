43d6d41a4c983f9f6190dfa3011d72fd6148a5c7
Valentina <oorhmo@gmail.com>
2022-11-25 15:10:10 +0100

    Alignment

--- src/Components/Form/Success.js
+++ src/Components/Form/Success.js
@@ -1,23 +1,25 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
 
-const Generate = () => {
+export class Success extends Component  {
+
+  render(){
+
   return (
     <div className="md:w-1/2 mx-auto shadow-xl rounded-2xl pb-2 bg-gray-100">
 
-    <div>
+    <div className="p-8 mx-32">
       <h2> Thank you trusting us </h2>
       <p> Your download is ready</p>
     </div>
-    <div>
+    <div className="mx-32">
     <Link to="/Download">
       <button
-      className="btn w-1/2"
-      onClick={this.continue}>Download</button>
+      className="btn w-1/2">Download</button>
       </Link>
     </div>
     </div>
   )
 }
-
-export default Generate;
+}
+export default Success;
