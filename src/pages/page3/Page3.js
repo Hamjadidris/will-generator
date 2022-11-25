97c80163f4dbf09583111d83325bffc85ae92c93
Hamjad Idris <dajmahsirdi@gmail.com>
2022-11-25 10:29:28 +0100

    feat: landing page

--- src/pages/page3/Page3.js
+++ src/pages/page3/Page3.js
@@ -1,9 +1,36 @@
 import React from 'react'
+import './page3.css'
+import { useState,useEffect } from 'react'
 import Download from '../../components/Download'
+import ClimbingBoxLoader from 'react-spinners/ClimbingBoxLoader'
+
 const Page3 = () => {
+    const [loading, setLoading] = useState(false) 
+
+    useEffect(() => {
+        setLoading(true)
+        setTimeout(() => {
+            setLoading(false)
+        }, 5000)
+    },[])
   return (
     <div>
-        {/* <Download /> */}
+        {
+            loading ?
+            (
+                <div className='loader'>
+
+                    <ClimbingBoxLoader
+                    color={'#dda15e'}
+                    loading={loading}
+                    size={20}
+                  />
+                  <h3>Generating your will</h3>
+                </div>
+            )
+            :
+                ('yoo')
+        }
     </div>
   )
 }
