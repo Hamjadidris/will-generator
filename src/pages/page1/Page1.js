0ecba13ea082cf1b8865faeb1198e8fd72097440
Hamjad Idris <dajmahsirdi@gmail.com>
2022-11-25 08:32:57 +0100

    feat: landing page

--- src/pages/page1/Page1.js
+++ src/pages/page1/Page1.js
@@ -1,12 +1,19 @@
 import React from 'react'
+import './page1.css'
+import family from '../../assets/family.jpg'
 import { Link } from 'react-router-dom';
 
 const Page1 = () => {
   return (
-    <div>
-        <h1>WILL GENERATOR</h1>
+    <div className='page1'>
+        <img alt='family' src={family} className='family'/>
+        <section className='intro'>
+            <h3>Plan for the rainy day</h3>
+            <h1>WILL GENERATOR</h1>
+            <h4>Just because you can't be with your family doesn't mean your property shouldn't </h4>
+        </section>
         <Link to="/info">
-            <button>To Page2</button>
+            <button className='generate-btn'>Start Generating</button>
          </Link>
     </div>
   )
